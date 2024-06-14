d0cd244350a2f19761261812ec1656b709aecbb8
itc-s23010 <s23010@std.it-college.ac.jp>
2024-06-14 09:58:14 +0900

    ギットイグノア

---
+++ d48Q6kadai.java
@@ -0,0 +1,9 @@
+//s23010
+//実行例を参考にd48Q6.javaをアレンジしたd48Q6kadai.javaを作成しましょう
+//実行例のように「チームAの勝利！」または「チームBの勝利！」または「引き分け！」を追加で表示するプログラムです
+
+
+class Q6 {
+	public static void main(String[] args){
+		if (args.length != 4)
+			System.out.println
