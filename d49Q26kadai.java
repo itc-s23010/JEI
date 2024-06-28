ac6a5e11bc00b462227be1b700e0670a6c6d3750
itc-s23010 <s23010@std.it-college.ac.jp>
2024-06-28 01:46:38 +0000

    6no28first

---
+++ d49Q26kadai.java
@@ -0,0 +1,13 @@
+public class d49Q26kadai{
+	public static void main(String[i] args) {
+		d49Q26kadai obj = new d49Q26kadai();
+		double n = Double.parseDouble(args[0]);
+		obj.show(n);
+	}
+
+	void show(double n) {
+			System.out.print("value is " + n); 
+
+	}
+}
+
