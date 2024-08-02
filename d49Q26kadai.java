fa699b7dc3e9c9c30e904a77e4567a622161ae7e
itc-s23010 <s23010@std.it-college.ac.jp>
2024-08-02 01:59:35 +0000

    Q26のコードをアレンジ

--- d49Q26kadai.java
+++ d49Q26kadai.java
@@ -1,13 +1,14 @@
-public class d49Q26kadai{
-	public static void main(String[i] args) {
-		d49Q26kadai obj = new d49Q26kadai();
-		double n = Double.parseDouble(args[0]);
-		obj.show(n);
-	}
+//s23010
+//数字「5.5」を実行例のように引数で指定するようにするコード
+import java.util.Scanner;
 
-	void show(double n) {
-			System.out.print("value is " + n); 
+public class d49Q26kadai {
+		public static void main(String[] args) {
+				d49Q26kadai obj = new d49Q26kadai();
+				obj.show(args[0]);
+		}
 
-	}
+		void show(String n) {
+				System.out.println("value is " + n);
+		}
 }
-
