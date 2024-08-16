648b5d686686083f02377f7262a859c7abd6fb6c
itc-s23010 <s23010@std.it-college.ac.jp>
2024-08-16 04:02:58 +0000

    mkdirD51

---
+++ D51/d51Q26kadai.java
@@ -0,0 +1,15 @@
+//s23010
+//「->」演算子を使ったプログラム
+
+public class d51Q26kadai {
+    public static void main(String[] args) {
+        int n = 1;
+        String s;
+        switch (n) {
+            case 1 -> s = "one";
+            case 2 -> s = "two";
+            default -> s = "?";
+        }
+        System.out.println(s);
+    }
+}
