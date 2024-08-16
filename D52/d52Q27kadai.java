d03756f0be9ca89a42f74fb00addb92fb1a399e8
itc-s23010 <s23010@std.it-college.ac.jp>
2024-08-16 08:31:22 +0000

    Runs correctly without compilation errors

---
+++ D52/d52Q27kadai.java
@@ -0,0 +1,18 @@
+//s23010
+//コンパイルエラーなく正しく実行できるプログラム
+
+enum Signal {
+		RED, YELLOW, GREEN;
+}
+public class d52Q27kadai {
+		public static void main(String[] args) {
+				Signal sig = Signal.YELLOW;
+				if (sig == Signal.GREEN) {
+						System.out.println("緑");
+				} else if (sig == Signal.YELLOW) {
+						System.out.println("黄");
+				} else {
+						System.out.println("赤");
+				}
+		}
+}
