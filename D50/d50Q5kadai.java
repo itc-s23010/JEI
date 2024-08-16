8227812c2a646a1044ce31498182d69f26ebae51
itc-s23010 <s23010@std.it-college.ac.jp>
2024-08-16 02:42:17 +0000

    d50Q5arrangement

---
+++ D50/d50Q5kadai.java
@@ -0,0 +1,33 @@
+//s23010
+//d50Q5kadaiのアレンジ
+
+public class d50Q5kadai {
+    public static void main(String[] args) {
+        int[] num = new int[args.length];
+
+        for (int i = 0; i< args.length; i++) {
+            num[i] = Integer.parseInt(args[i]);
+        }
+
+        int tmp;
+        for (int i = 1;i < num.length; i++) {
+            for(int j = i - 1; j >= 0 && num[j] > num[j + 1]; j--) {
+                tmp = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = tmp;
+                String result = "";
+                for(int n : num) {
+                    result += String.valueOf(n);
+                }
+                System.out.println("ソート途中:" + result);
+            }
+        }
+		System.out.println("-----------------");
+		System.out.print("ソート結果:");
+
+        for(int val : num) {
+				System.out.print(val + " ");
+        }
+		System.out.println("");
+    }
+}
